0d378bfc2794d8219edf308ceac8bf9516f20283
gaolianghu123 <122991463+gaolianghu123@users.noreply.github.com>
2025-04-19 19:56:48 +0800

    Update config.js

--- themes/heo/config.js
+++ themes/heo/config.js
@@ -6,11 +6,11 @@ const CONFIG = {
 
   HEO_SITE_CREATE_TIME: '2025-04-16', // 建站日期，用于计算网站运行的第几天
 
-  // 首页顶部通知条滚动内容，如不需要可以留空 []
-  HEO_NOTICE_BAR: [
-    { title: '欢迎来到我的博客', url: 'https://blog.tangly1024.com' },
-    { title: '访问文档中心获取更多帮助', url: 'https://docs.tangly1024.com' }
-  ],
+  // // 首页顶部通知条滚动内容，如不需要可以留空 []
+  // HEO_NOTICE_BAR: [
+  //   { title: '欢迎来到我的博客', url: 'https://blog.tangly1024.com' },
+  //   { title: '访问文档中心获取更多帮助', url: 'https://docs.tangly1024.com' }
+  // ],
 
   // 英雄区左右侧组件颠倒位置
   HEO_HERO_REVERSE: true,
@@ -18,14 +18,14 @@ const CONFIG = {
   HEO_HERO_BODY_REVERSE: true,
 
   // 英雄区(首页顶部大卡)
-  HEO_HERO_TITLE_1: '分享编程',
-  HEO_HERO_TITLE_2: '与思维认知',
-  HEO_HERO_TITLE_3: 'TANGLY1024.COM',
-  HEO_HERO_TITLE_4: '新版上线',
-  HEO_HERO_TITLE_5: 'NotionNext4.0 轻松定制主题',
-  HEO_HERO_TITLE_LINK: 'https://tangly1024.com',
+  HEO_HERO_TITLE_1: '',
+  HEO_HERO_TITLE_2: '',
+  HEO_HERO_TITLE_3: '',
+  HEO_HERO_TITLE_4: '',
+  HEO_HERO_TITLE_5: '',
+  HEO_HERO_TITLE_LINK: 'https://www.youtube.com/@Vc%E5%B7%A5%E5%85%B7%E7%AE%B1',
   // 英雄区遮罩文字
-  HEO_HERO_COVER_TITLE: '随便逛逛',
+  HEO_HERO_COVER_TITLE: '随机传送',
 
   // 英雄区显示三个置顶分类
   HEO_HERO_CATEGORY_1: { title: '必看精选', url: '/tag/必看精选' },
